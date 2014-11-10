3481606311647cb402c12ba16c9f024b4811a8c4
kmain4 <kmain4@fau.edu>
2014-11-10 11:45:25 -0500

    Update CfgHints.hpp

--- includes/CfgHints.hpp
+++ includes/CfgHints.hpp
@@ -31,5 +31,25 @@ class CfgHints
 			image = "\a3\ui_f\data\gui\cfg\hints\ArtilleryCall_ca.paa";
 			noImage = false;
 		};
+		class casRequest
+		{
+			displayName = "CAS Request";
+			displayNameShort = "Requesting CAS";
+			description = "CAS is a precise strike effective against well defended targets.%1<t></t>1. Point at the target.%1<t></t>2. Press %3[0]%4 to enter the command menu.%1<t></t>3. Select %3Communications%4 - %3[8]%4.%1<t></t>4. Select %CAS Strike%4.%1<t></t> The pilot will then confirm the request and execute the order. <t></t> If the coordinates that you sent are unable to be used, the supporting unit will grant you another request.";
+			tip = "Make sure there are no friendly forces in the vicinity of your target. You will be held accountable for friendly fire incidents.";
+			arguments[] = {};
+			image = "\a3\ui_f\data\gui\cfg\hints\ArtilleryCall_ca.paa";
+			noImage = false;
+		};
+		class qrfRequest
+		{
+			displayName = "QRF Request";
+			displayNameShort = "Requesting a QRF";
+			description = "A quick reaction force is an easily mobilized, mechanized fire team.%1<t></t>1. Point at the target.%1<t></t>2. Press %3[0]%4 to enter the command menu.%1<t></t>3. Select %3Communications%4 - %3[8]%4.%1<t></t>4. Select %QRF Request%4.%1<t></t> Command will then confirm the request and dispatch the friendly forces to the requested location. <t></t> If the coordinates that you sent are unable to be used, the supporting unit will grant you another request.";
+			tip = "These forces will not be reinforced once they are no longer combat effective, and players may not get in QRF vehicles.";
+			arguments[] = {};
+			image = "\a3\ui_f\data\gui\cfg\hints\ArtilleryCall_ca.paa";
+			noImage = false;
+		};
 	};
 };
