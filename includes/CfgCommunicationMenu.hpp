584b93630af0f00f3ef4b48b60ac46cfbe48c743
kmain4 <kmain4@fau.edu>
2014-11-15 14:23:48 -0500

    Update CfgCommunicationMenu.hpp

--- includes/CfgCommunicationMenu.hpp
+++ includes/CfgCommunicationMenu.hpp
@@ -43,11 +43,21 @@ class CfgCommunicationMenu
         removeAfterExpressionCall = 1; // 1 to remove the item after calling
     };
     
-    class qrf
+    class qrfRequest
     {
         text = "Quick Reaction Force"; // Text displayed in the menu and in a notification
         submenu = ""; // Submenu opened upon activation (expression is ignored when submenu is not empty.)
-        expression = "player setVariable ['BIS_SUPP_request', ['Artillery', _pos]];"; // Code executed upon activation
+        expression = "_this spawn KOL_fnc_qrfRequest;"; // Code executed upon activation
+        icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\artillery_ca.paa"; // Icon displayed permanently next to the command menu
+        cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
+        enable = "1"; // Simple expression condition for enabling the item
+        removeAfterExpressionCall = 1; // 1 to remove the item after calling
+    };
+	class ugvRequest
+    {
+        text = "UGV Deployment"; // Text displayed in the menu and in a notification
+        submenu = ""; // Submenu opened upon activation (expression is ignored when submenu is not empty.)
+        expression = "_this spawn KOL_fnc_ugvRequest"; // Code executed upon activation
         icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\artillery_ca.paa"; // Icon displayed permanently next to the command menu
         cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
         enable = "1"; // Simple expression condition for enabling the item
